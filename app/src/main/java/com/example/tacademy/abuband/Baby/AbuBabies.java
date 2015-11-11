db4fad04ced53adad8e35c38a3e186ee690fdd1f
Yoonho <xpzkaos0@gmail.com>
2015-11-11 17:09:30 +0900

    BabyLisy Network2

---
+++ app/src/main/java/com/example/tacademy/abuband/Baby/AbuBabies.java
@@ -0,0 +1,13 @@
+package com.example.tacademy.abuband.Baby;
+
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by dongja94 on 2015-10-19.
+ */
+public class AbuBabies {
+    int code;
+    ArrayList<BabyItemData> result;
+}
